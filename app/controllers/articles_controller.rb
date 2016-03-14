44c8bbcea2dc4d2aef125c1e467874976dfb8b52
hector peredo <hectorda@hotmail.es>
2016-03-13 22:13:51 -0300

    Listo Crud

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,18 +14,20 @@ class ArticlesController < ApplicationController
 	def new
 		@article = Article.new;
 	end
+	#GET /articles/:id/edit
+	def edit
+		@article = Article.find(params[:id])
+	end
 
 	#POST /articles
 	def create
-		@article = Article.new(title: params[:article][:title],
-								body: params[:article][:body])
+		@article = Article.new(article_params)
 		
 		if @article.save
 			redirect_to @article
 		else
 			render :new
-		end
-		
+		end		
 	end
 
 	def destroy
@@ -37,7 +39,18 @@ class ArticlesController < ApplicationController
 	#Get /articles/:id/update
 	def update
 		# @article.update_attributs({title: 'Nuevo Articulo'})
+		@article = Article.find(params[:id])
+		if(@article.update(article_params))
+			redirect_to @article
+		else
+			render :edit
+		end
 	end
 
+	private
+
+	def article_params
+		params.require(:article).permit(:title,:body,:visits_count)
+	end
 
 end
