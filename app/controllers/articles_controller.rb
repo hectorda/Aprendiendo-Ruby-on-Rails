5130941104577407b56c5dbc6b4bbcfa37f6fcd1
hectorda <hectorda@hotmail.es>
2016-03-14 11:49:10 -0300

    Scaffold

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+	before_action :authenticate_user!, except: [:show,:index]
+	before_action :set_article, except: [:index,:new,:create]
 	#Get /articles
 	def index
 		#Todos los Registros
@@ -6,8 +8,7 @@ class ArticlesController < ApplicationController
 	end
 	#Get article/:id
 	def show
-		#Encontrar un registro por id
-		@article = Article.find(params[:id])
+		@article.update_visits_count
 	end
 
 	#Get /articles/new
@@ -16,12 +17,11 @@ class ArticlesController < ApplicationController
 	end
 	#GET /articles/:id/edit
 	def edit
-		@article = Article.find(params[:id])
 	end
 
 	#POST /articles
 	def create
-		@article = Article.new(article_params)
+		@article = current_user.articles.new(article_params)
 		
 		if @article.save
 			redirect_to @article
@@ -31,15 +31,12 @@ class ArticlesController < ApplicationController
 	end
 
 	def destroy
-		@article = Article.find(params[:id])
 		@article.destroy #Elimina el objeto de la base de datos
 		redirect_to articles_path
 	end
 
 	#Get /articles/:id/update
 	def update
-		# @article.update_attributs({title: 'Nuevo Articulo'})
-		@article = Article.find(params[:id])
 		if(@article.update(article_params))
 			redirect_to @article
 		else
@@ -48,6 +45,11 @@ class ArticlesController < ApplicationController
 	end
 
 	private
+
+	def set_article
+		@article = Article.find(params[:id])
+
+	end
 
 	def article_params
 		params.require(:article).permit(:title,:body,:visits_count)
